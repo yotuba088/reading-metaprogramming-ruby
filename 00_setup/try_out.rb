ae31bb54cf8bf4993729b0df62135ed563fcb723
yotuba088 <47002817+yotuba088@users.noreply.github.com>
2020-02-21 18:18:23 +0900

    2章まで

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,6 +5,9 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+
+  attr_writer :first_name
+
   def initialize(first_name, middle_name="", last_name)
     @first_name = first_name
     @middle_name = middle_name
@@ -15,7 +18,7 @@ class TryOut
     [@first_name, @middle_name, @last_name].reject(&:empty?).join(' ')
   end
 
-  def first_name(replace_name)
+  def first_name (replace_name)
     first_name = replace_name
   end
 
@@ -24,14 +27,6 @@ class TryOut
   end
 
   def upcase_full_name!
-    upcase_names!
-    full_name
-  end
-
-  private
-  attr_reader :first_name, :middle_name, :last_name
-
-  def upcase_names!
     @first_name.upcase!
     @middle_name.upcase!
     @last_name.upcase!
