73745dee7ab27cc5dee6330851a303e931509cee
yotuba088 <47002817+yotuba088@users.noreply.github.com>
2020-03-25 12:50:59 +0900

    try_over3_3を解いた

--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -6,6 +6,20 @@ TryOver3 = Module.new
 # - `test_` メソッドがこのクラスに実装されていなくても `test_` から始まるメッセージに応答することができる
 # - TryOver3::A1 には `test_` から始まるインスタンスメソッドが定義されていない
 
+class TryOver3::A1
+  def run_test
+    nil
+  end
+
+  def method_missing(name, *args)
+    if name.to_s.start_with?("test_")
+      run_test
+    else
+      super
+    end
+  end
+end
+
 
 # Q2
 # 以下要件を満たす TryOver3::A2Proxy クラスを作成してください。
@@ -15,6 +29,20 @@ class TryOver3::A2
   def initialize(name, value)
     instance_variable_set("@#{name}", value)
     self.class.attr_accessor name.to_sym unless respond_to? name.to_sym
+  end
+end
+
+class TryOver3::A2Proxy
+  def initialize(source)
+    @source = source
+  end
+
+  def method_missing(name, *args)
+    if @source.methods.include?(name)
+      @source.send(name, *args)
+    else
+      super
+    end
   end
 end
 
@@ -35,7 +63,10 @@ module TryOver3::OriginalAccessor2
           self.class.define_method "#{attr_sym}?" do
             @attr == true
           end
+        elsif ![true, false].include?(value) && respond_to?("#{attr_sym}?")
+          self.class.undef_method "#{attr_sym}?"
         end
+
         @attr = value
       end
     end
@@ -49,6 +80,25 @@ end
 # TryOver3::A4::Hoge.run
 # # => "run Hoge"
 
+class TryOver3::A4
+  self.runners=(args)
+    args.each do |name|
+      self.const_get(name)
+    end
+  end
+
+  def self.const_missing(name)
+    super if name != :Hoge
+
+    klass = Class.new
+
+    klass.define_singleton_method(:run) do
+      "run #{name.to_s}"
+    end
+
+    klass
+  end
+end
 
 # Q5. チャレンジ問題！ 挑戦する方はテストの skip を外して挑戦してみてください。
 #
