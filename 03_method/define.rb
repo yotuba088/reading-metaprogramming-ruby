e80670943fce8c87e500c8ccf3bf1409e5544d0d
yotuba088 <47002817+yotuba088@users.noreply.github.com>
2020-03-25 12:50:48 +0900

    defineを解いた

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -2,6 +2,12 @@
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
 
+class A1
+  define_method("//") do
+    "//"
+  end
+end
+  
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -10,7 +16,35 @@
 #   - 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
 
+class A2
+  def initialize(method_prefix)
+    method_prefix.each do |prefix|
+      method_name = "hoge_#{prefix}"
+      self.class.send(:define_method, method_name) do |*args|
+        return dev_team if args[0].nil?
+
+        method_name * args.sum
+      end
+    end
+  end
+
+  def dev_team
+    "SmartHR Dev Team"
+  end
+end
+
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+module OriginalAccessor
+  def self.included(klass)
+    klass.class_eval do 
+      def self.my_attr_accessor(attr_name)
+        define_method(attr_name) do
+        end
+      end
+    end
+  end
+end
